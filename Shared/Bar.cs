607880275bb3d02ed26204035f8f046514e4525e
TimofeyHQ <timonhztv@gmail.com>
2021-10-25 23:58:07 +0300

    Added Bar and Force classes

---
+++ Shared/Bar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarsWebApp.Shared
+{
+    public class Bar
+    {
+        private Force leftConcentrated;
+        private Force rightConcentrated;
+        private Force distributed;
+        public decimal Length;
+        public decimal Area;
+        public decimal Material;
+        public Force LeftConcentrated
+        {
+            get
+            {
+                return leftConcentrated;
+            }
+            set
+            {
+                if (value.Type != Force.ForceType.q)
+                    leftConcentrated = value;
+            }
+        }
+        public Force RightConcentrated
+        {
+            get
+            {
+                return rightConcentrated;
+            }
+            set
+            {
+                if (value.Type != Force.ForceType.q)
+                    rightConcentrated = value;
+            }
+        }
+        public Force Distributed
+        {
+            get
+            {
+                return distributed;
+            }
+            set
+            {
+                if (value.Type == Force.ForceType.q)
+                    distributed = value;
+            }
+        }
+    }
+}
---
+++ Shared/Force.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarsWebApp.Shared
+{
+    public class Force
+    {
+        public enum ForceType
+        {
+            F, q, M
+        };
+        public ForceType Type { get; private set; }
+        public decimal Scale { get; private set; }
+    }
+}
