9dc51a6b2c8b98c3204b1297a0be9cd187b7c0cb
TimofeyHQ <timonhztv@gmail.com>
2021-10-26 01:15:34 +0300

    Added BarSystem class.

--- Shared/Bar.cs
+++ Shared/Bar.cs
@@ -14,6 +14,16 @@ namespace BarsWebApp.Shared
         public decimal Length;
         public decimal Area;
         public decimal Material;
+        public Bar(decimal length, decimal area, decimal material) 
+        {
+            if (length > 0 && area > 0 && material > 0)
+            {
+                Length = length;
+                Area = area;
+                Material = material;
+            }
+            else throw new IndexOutOfRangeException("Incorrect characteristics for bar.");
+        }
         public Force LeftConcentrated
         {
             get
---
+++ Shared/BarSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarsWebApp.Shared;
+
+namespace BarWebApp.Shared
+{
+    class BarSystem
+    {
+        private List<Bar> bars;
+        public bool HasLeftBorder = false;
+        public bool HasRightBorder = false;
+        public Bar AddtBar(decimal len, decimal area, decimal material)
+        {
+            if (len > 0 && area > 0 && material > 0)
+            {
+                var newBar = new Bar(len, area, material);
+                if (HasLeftBorder && HasRightBorder == false)
+                    HasLeftBorder = true;
+                bars.Add(newBar);
+                return newBar;
+            }
+            else return null;
+        }
+        public void RemoveLeftBar(string direction)
+        {
+            if (direction == "left")
+                if (bars != null)
+                    bars.Remove(bars.First());
+            if (direction == "right")
+                if (bars != null)
+                    bars.Remove(bars.Last());
+        }
+    }
+}
